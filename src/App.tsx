9b956f95a08cbb471ce0856351e96672303349dc
buihieu05 <buihieu12345678905@gmail.com>
2026-06-30 17:49:04 +0700

    lab1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import { Modal } from "antd";
+import { useState } from "react";
+import { Layout } from "antd";
+import { Table } from "antd";
 
 function App() {
+  const { Header, Content, Footer } = Layout;
+  
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  const [open, setOpen] = useState(false);
+
+
+
+  const columns = [
+  { title: "Name", dataIndex: "name" },
+  { title: "Email", dataIndex: "email" },
+  { title: "Role", dataIndex: "role" },
+];
+
+const data = [
+  { key: 1, name: "John", email: 251, role: "nh" },
+  { key: 2, name: "Anna", email: 301, role: "nh" },
+]
+  
+
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -35,7 +62,66 @@ function App() {
 
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
-        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+        <Button type="primary">Login</Button>
+        <Button>Text</Button>
+       <Form onFinish={onFinish}>
+      <Form.Item
+      label = "name"
+        name="name"
+        rules={[{ required: true, message: "Nhập Name" }]}
+      >
+        <Input placeholder="Name" />
+      </Form.Item>
+
+      <Form.Item
+      label = "email"
+        name="email"
+        rules={[{ required: true, message: "Nhập email" }]}
+      >
+        <Input placeholder="Email" />
+      </Form.Item>
+
+      <Form.Item
+      label = "password"
+        name="password"
+        rules={[{ required: true, message: "Nhập Password" }]}
+      >
+        <Input placeholder="Password" />
+      </Form.Item>
+
+      
+
+      <Form.Item>
+        <Button htmlType="submit" type="primary">
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+
+
+
+    <Button onClick={() => setOpen(true)}>Add User</Button>
+
+      <Modal
+        open={open}
+        onCancel={() => setOpen(false)}
+        onOk={() => setOpen(false)}
+      >
+        Nội dung modal
+      </Modal>
+
+
+       <Layout>
+      <Header style={{ color: "white" }}>Header</Header>
+      <Content style={{ padding: 20 }}>Content</Content>
+      <Footer>Sidebar</Footer>
+    </Layout>
+
+
+    <Table columns={columns} dataSource={data} />
+
+        
+
       </div>
 
       <Toaster />
